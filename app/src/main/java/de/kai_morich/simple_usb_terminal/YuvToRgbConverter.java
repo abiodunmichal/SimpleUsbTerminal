a6ab4012de15aaad6cbbefdbe8c2b59d44ad09ed
abiodunmichal <abiodunmichal909@gmail.com>
2025-07-30 15:22:23 +0100

    Update YuvToRgbConverter.java

--- app/src/main/java/de/kai_morich/simple_usb_terminal/YuvToRgbConverter.java
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/YuvToRgbConverter.java
@@ -4,31 +4,23 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
 import android.graphics.PixelFormat;
+import android.graphics.YuvImage;
 import android.media.Image;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicYuvToRGB;
-import android.util.Log;
+import android.os.Build;
 
-import androidx.camera.core.ImageProxy;
+import androidx.annotation.RequiresApi;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
 
 public class YuvToRgbConverter {
-    private final RenderScript rs;
-    private final ScriptIntrinsicYuvToRGB yuvToRgbIntrinsic;
+    public YuvToRgbConverter(Context context) {}
 
-    public YuvToRgbConverter(Context context) {
-        rs = RenderScript.create(context);
-        yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs));
-    }
-
-    public void yuvToRgb(ImageProxy image, Bitmap output) {
-        ImageProxy.PlaneProxy[] planes = image.getPlanes();
-        ByteBuffer yBuffer = planes[0].getBuffer();
-        ByteBuffer uBuffer = planes[1].getBuffer();
-        ByteBuffer vBuffer = planes[2].getBuffer();
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void yuvToRgb(Image image, Bitmap output) {
+        ByteBuffer yBuffer = image.getPlanes()[0].getBuffer();
+        ByteBuffer uBuffer = image.getPlanes()[1].getBuffer();
+        ByteBuffer vBuffer = image.getPlanes()[2].getBuffer();
 
         int ySize = yBuffer.remaining();
         int uSize = uBuffer.remaining();
@@ -40,15 +32,15 @@ public class YuvToRgbConverter {
         vBuffer.get(nv21, ySize, vSize);
         uBuffer.get(nv21, ySize + vSize, uSize);
 
-        Allocation in = Allocation.createSized(rs, Element.U8(rs), nv21.length);
-        Allocation out = Allocation.createFromBitmap(rs, output);
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new android.graphics.Rect(0, 0, image.getWidth(), image.getHeight()), 100, out);
+        byte[] imageBytes = out.toByteArray();
 
-        in.copyFrom(nv21);
-        yuvToRgbIntrinsic.setInput(in);
-        yuvToRgbIntrinsic.forEach(out);
-        out.copyTo(output);
-
-        in.destroy();
-        out.destroy();
+        Bitmap decoded = android.graphics.BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        if (decoded != null) {
+            android.graphics.Canvas canvas = new android.graphics.Canvas(output);
+            canvas.drawBitmap(decoded, 0, 0, null);
+        }
     }
-}
+    }
